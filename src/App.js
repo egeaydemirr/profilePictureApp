7d3ea843134c3c6b2eed3c523b39e92f391c98ae
Ege Aydemir <egeaydemirr97@gmail.com>
2023-04-05 02:09:44 +0300

    Files

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './screens/Home';
+import Result from './screens/Result';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Result" component={Result} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
