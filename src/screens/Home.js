bfde57c14b85ac7bc7aa384fe31c37292b7784ba
Ege Aydemir <egeaydemirr97@gmail.com>
2023-04-05 02:23:52 +0300

    TouchableOpacity and styles of button

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,42 @@
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native';
 import React from 'react';
 
-// This is the Home screen
+//TODO: Add a touchable opacity for a image
+//TODO: onPress. actionSheet will open and 4 options will be displayed
+// TODO: 1. Camera
+// TODO: 2. Gallery
+// TODO: 3. Folder
+// TODO: 4. Cancel
+
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <TouchableOpacity>
+        <View style={styles.imageContainer}>
+          <Image source={require('../assets/gallery.jpg')} />
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    borderWidth: 1,
+    elevation: 2,
+    marginBottom: 200,
+  },
+});
 
 export default Home;
